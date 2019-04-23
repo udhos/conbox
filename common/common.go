597aa4d23f38088df1655fe32bbd74faef2c8dac
udhos <udhos>
2019-04-23 19:50:57 -0300

    Lint.

--- common/common.go
+++ common/common.go
@@ -7,6 +7,7 @@ import (
 
 const version = "0.0"
 
+// ShowVersion prints conbox version.
 func ShowVersion() {
 	fmt.Printf("conbox: version %s runtime %s GOMAXPROC=%d OS=%s ARCH=%s\n", version, runtime.Version(), runtime.GOMAXPROCS(0), runtime.GOOS, runtime.GOARCH)
 }
