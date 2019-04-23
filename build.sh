79d5552a21f26bf3e87c8ac2a690ab60cfdb071c
udhos <udhos>
2019-04-23 14:27:59 -0300

    Lint.

--- build.sh
+++ build.sh
@@ -4,9 +4,23 @@ msg() {
 	echo 2>&1 "$0": $@
 }
 
-hash gosimple 2>/dev/null && gosimple ./applets
-hash golint 2>/dev/null && golint ./applets
-hash staticcheck 2>/dev/null && staticcheck ./applets
+if [ -n "$1" ]; then
+	lint=1
+else
+	lint=
+fi
+
+if [ -n "$lint" ]; then
+	msg linting
+else
+	msg NOT linting
+fi
+
+if [ -n "$lint" ]; then
+	hash gosimple 2>/dev/null && gosimple ./applets
+	hash golint 2>/dev/null && golint ./applets
+	hash staticcheck 2>/dev/null && staticcheck ./applets
+fi
 
 gofmt -s -w ./applets
 go fix ./applets/...
@@ -14,9 +28,11 @@ go vet -vettool="$(which shadow)" ./applets/...
 go test ./applets/...
 go install -v ./applets/...
 
-#hash gosimple 2>/dev/null && gosimple ./conbox
-#hash golint 2>/dev/null && golint ./conbox
-#hash staticcheck 2>/dev/null && staticcheck ./conbox
+if [ -n "$lint" ]; then
+	hash gosimple 2>/dev/null && gosimple ./conbox
+	hash golint 2>/dev/null && golint ./conbox
+	hash staticcheck 2>/dev/null && staticcheck ./conbox
+fi
 
 gofmt -s -w ./conbox
 go fix ./conbox
